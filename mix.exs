3083f47c434e6e1f4515b4305847eb903bef8005
Jon <jonchoukroun@gmail.com>
2018-06-20 19:59:05 -0700

    Restore version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WilliamStorckPhx.Mixfile do
   def project do
     [
       app: :william_storck_phx,
-      version: "0.0.2",
+      version: "0.0.1",
       elixir: "~> 1.6.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
