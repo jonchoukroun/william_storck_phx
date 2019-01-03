d08ce55c7bcb8371a0a55dfbdd2a2925bf3637e1
Jon <jonchoukroun@gmail.com>
2019-01-03 12:08:09 -0800

    Set elixir v 1.7.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WilliamStorckPhx.Mixfile do
     [
       app: :william_storck_phx,
       version: "0.0.1",
-      elixir: "~> 1.6.4",
+      elixir: "~> 1.7.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
