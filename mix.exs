1d4bd28039b6eaf6bfdd66fc119025330c5b6f1c
Jon <jonchoukroun@gmail.com>
2019-01-06 16:11:43 -0800

    Fix typo; increment to 1st minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WilliamStorckPhx.Mixfile do
   def project do
     [
       app: :william_storck_phx,
-      version: "0.0.2",
+      version: "0.1.0",
       elixir: "~> 1.7.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
