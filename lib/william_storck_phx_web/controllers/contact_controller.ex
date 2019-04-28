4131c5d135318ffe4f5b4583d958f96b04d0b6e2
Jon <jonchoukroun@gmail.com>
2019-04-27 18:50:18 -0700

    Link to contact from available paintings and include name in emails

--- lib/william_storck_phx_web/controllers/contact_controller.ex
+++ lib/william_storck_phx_web/controllers/contact_controller.ex
@@ -2,13 +2,12 @@ defmodule WilliamStorckPhxWeb.ContactController do
   use WilliamStorckPhxWeb, :controller
   alias SendGrid.Email
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
+  def index(conn, %{"painting" => painting}), do: render conn, "index.html", painting: painting
+  def index(conn, params), do: render conn, "index.html"
 
   def capture_email(conn, params) do
     contact = params["contact"]
-    case build_and_send_email(contact["email"], contact["name"], contact["message"]) do
+    case send_email(contact["email"], contact["name"], contact["message"], contact["painting"]) do
       :ok ->
         render conn, "index.html", success: true
 
@@ -21,16 +20,19 @@ defmodule WilliamStorckPhxWeb.ContactController do
     end
   end
 
-  defp build_and_send_email(email, _name, _message) when is_nil(email), do: :error
+  defp send_email(email, _name, _message, _painting) when is_nil(email), do: :error
 
-  defp build_and_send_email(email, name, message) do
+  defp send_email(email, name, message, painting) do
     Email.build()
     |> Email.add_to("swancovestudio@gmail.com")
     |> Email.put_from(email)
-    |> Email.put_subject("New message from #{name}")
+    |> Email.put_subject(write_subject(name, painting))
     |> Email.put_text(message)
     |> SendGrid.Mailer.send()
   end
+
+  defp write_subject(name, painting) when is_nil(painting), do: "New message from #{name}"
+  defp write_subject(name, painting), do: "#{name} is interested in #{painting}"
 
   defp parse_error(_reason) do
     "Sorry, something went wrong. Please try again later."
