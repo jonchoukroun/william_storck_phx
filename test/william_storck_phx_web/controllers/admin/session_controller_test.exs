cadf63cc09e7eb1acbd29c9b9db2406ac6007597
Jon <jonchoukroun@gmail.com>
2019-06-29 20:06:14 -0700

    Ensure delete session action removes current user id from session

--- test/william_storck_phx_web/controllers/admin/session_controller_test.exs
+++ test/william_storck_phx_web/controllers/admin/session_controller_test.exs
@@ -1,17 +1,22 @@
 defmodule WilliamStorckPhxWeb.Admin.SessionControllerTest do
   use WilliamStorckPhxWeb.ConnCase
 
-  @create_attrs %{email: "some email", password: "some password"}
-  @invalid_attrs %{email: nil, password: nil}
+  import Plug.Test
+
+  alias WilliamStorckPhx.Auth
+
+  @user_attrs %{email: "some email", name: "some name", password: "some password"}
+  @valid_attrs %{email: @user_attrs.email, password: @user_attrs.password}
+  @invalid_attrs %{email: nil, name: nil, password: nil}
 
   def fixture(:user) do
-    {:ok, user} = WilliamStorckPhx.Auth.create_user(@create_attrs)
+    {:ok, user} = Auth.create_user(@user_attrs)
     user
   end
 
   describe "create session" do
     test "redirects to admin landing page when data is valid", %{conn: conn} do
-      conn = post(conn, admin_session_path(conn, :create), session: @create_attrs)
+      conn = post(conn, admin_session_path(conn, :create), session: @valid_attrs)
 
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == admin_session_path(conn, :show, id)
@@ -27,9 +32,22 @@ defmodule WilliamStorckPhxWeb.Admin.SessionControllerTest do
   end
 
   describe "delete session" do
-    test "deletes chosen session", %{conn: conn} do
-      conn = delete(conn, admin_session_path(conn, :delete), session: get_session(conn, :current_user_id))
-      assert redirected_to(conn) == admin_user_path(conn, :index)
+    setup [:sign_in_user]
+
+    test "deletes chosen session", %{conn: conn, user: user} do
+      assert get_session(conn, :current_user_id) === user.id
+
+      conn = delete(conn, admin_session_path(conn, :delete))
+      refute get_session(conn, :current_user_id)
     end
+  end
+
+  defp sign_in_user(_) do
+    user = fixture(:user)
+
+    conn = build_conn(:get, "/admin")
+    |> init_test_session(current_user_id: user.id)
+
+    {:ok, conn: conn, user: user}
   end
 end
