19ea9dfa53c43d8e869688a43ebcd47215f805d6
Jon <jonchoukroun@gmail.com>
2019-06-29 23:20:50 -0700

    Mount current user on conn assign if user id in session

---
+++ test/william_storck_phx_web/plugs/authenticate_test.exs
@@ -0,0 +1,41 @@
+defmodule WilliamStorckPhxWeb.Plug.RequireLoginTest do
+  use WilliamStorckPhxWeb.ConnCase
+
+  import Plug.Test, only: [init_test_session: 2]
+
+  alias WilliamStorckPhxWeb.Plugs.Authenticate
+
+  describe "when user id in session" do
+    setup [:create_user_session]
+
+    test "populates conn assigns with user", %{conn: conn, user: user} do
+      assert get_session(conn, :current_user_id)
+
+      conn = conn |> Authenticate.call(%{})
+      assert conn.assigns[:current_user].id === user.id
+    end
+  end
+
+  describe "when no user id in session" do
+    test "does nothing" do
+      conn = build_conn(:get, "/")
+      conn = get(conn, admin_landing_path(conn, :index))
+      |> Authenticate.call(%{})
+
+      refute get_session(conn, :current_user_id)
+      refute conn.assigns[:current_user]
+    end
+  end
+
+  defp create_user_session(_) do
+    {:ok, user} = WilliamStorckPhx.Auth.create_user(%{
+      name: "bob",
+      email: "bob@aol.com",
+      password: "xxxxxx"})
+
+    conn = build_conn(:get, "/admin")
+    |> init_test_session(current_user_id: user.id)
+
+    {:ok, conn: conn, user: user}
+  end
+end
