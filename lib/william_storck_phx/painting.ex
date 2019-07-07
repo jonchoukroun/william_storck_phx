097017d00ecb0c13bf0a7ca1a740f530d41c07b2
Jon <jonchoukroun@gmail.com>
2019-07-07 12:17:36 -0700

    Manage param/field validation; call upload service and slug generator via pipeline

--- lib/william_storck_phx/painting.ex
+++ lib/william_storck_phx/painting.ex
@@ -1,19 +1,29 @@
 defmodule WilliamStorckPhx.Painting do
   use Ecto.Schema
+
   import Ecto.Changeset
 
+  alias WilliamStorckPhx.{UploadService, SlugGenerator}
 
   schema "paintings" do
-    field :slug
-    field :featured, :boolean, default: false
-    field :height, :integer
-    field :material, :string
     field :name, :string
-    field :size, :string
-    field :src, :string
-    field :status, :string
-    field :price, :integer
+    field :material, :string
+
+    field :painting_height, :integer, virtual: true
+    field :painting_width, :integer, virtual: true
+    field :height, :integer
     field :width, :integer
+
+    field :price, :integer
+    field :status, :string
+
+    field :image_file, :any, virtual: true
+    field :src, :string
+
+    field :size, :string
+    field :slug
+
+    field :featured, :boolean, default: false
 
     timestamps()
   end
@@ -22,7 +32,45 @@ defmodule WilliamStorckPhx.Painting do
   def changeset(painting, attrs) do
     painting
     |> cast(attrs, [
-      :slug, :name, :src, :material, :size, :status, :price, :featured, :height, :width])
-    |> validate_required([:name, :src, :material, :size, :status, :featured, :height, :width])
+      :name, :material, :painting_height, :painting_width, :price, :status, :image_file])
+    |> validate_required([
+      :name, :material, :painting_height, :painting_width, :status, :image_file])
+    |> upload_painting()
+    |> cast(attrs, [:src, :height, :width])
+    |> validate_required([:src, :height, :width])
+    |> change(image_file: nil)
+    |> format_size()
+    |> cast(attrs, [:size])
+    |> validate_required([:size])
+    |> change(%{painting_height: nil, painting_width: nil})
+    |> generate_slug()
+    |> cast(attrs, [:slug])
+    |> validate_required([:slug])
   end
+
+  defp upload_painting(%Ecto.Changeset{valid?: true,
+  changes: %{name: name, image_file: image_file}} = changeset) do
+    with %{src: _src, height: _height, width: _width} = changes <- UploadService.upload_file(
+    %{name: name, file: image_file}) do
+      change(changeset, changes)
+    else
+      {:error, msg} ->
+        add_error(changeset, :src, "Upload failed: #{msg}")
+    end
+  end
+
+  defp upload_painting(changeset), do: changeset
+
+  defp format_size(%Ecto.Changeset{valid?: true,
+  changes: %{painting_height: height, painting_width: width}} = changeset) do
+    change(changeset, size: "#{height}\" x #{width}\"")
+  end
+
+  defp format_size(changeset), do: changeset
+
+  defp generate_slug(%Ecto.Changeset{valid?: true, changes: %{name: name}} = changeset) do
+    change(changeset, slug: SlugGenerator.generate_slug(name))
+  end
+
+  defp generate_slug(changeset), do: changeset
 end
