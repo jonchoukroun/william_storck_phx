229a1f44e31aa0bc2b4a6c9afb1ccde6874cad77
Jon <jonchoukroun@gmail.com>
2019-07-07 15:49:33 -0700

    Increment version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WilliamStorckPhx.Mixfile do
   def project do
     [
       app: :william_storck_phx,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.8.2",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
