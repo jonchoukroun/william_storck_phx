9a2d4eec2637a7f458804988972d14c230a2f7dd
Jon <jonchoukroun@gmail.com>
2019-07-14 19:18:30 -0700

    Ensure authentication before category admin

--- test/william_storck_phx_web/controllers/admin/category_controller_test.exs
+++ test/william_storck_phx_web/controllers/admin/category_controller_test.exs
@@ -1,9 +1,10 @@
 defmodule WilliamStorckPhxWeb.Admin.CategoryControllerTest do
   use WilliamStorckPhxWeb.ConnCase
 
-  alias WilliamStorckPhx.Admin
+  alias WilliamStorckPhx.{Admin, Auth}
   alias WilliamStorckPhxWeb.Router.Helpers, as: Routes
 
+  @admin_attrs %{email: "admin@aol.com", name: "admin", password: "xxxxxx"}
   @create_attrs %{name: "some name"}
   @update_attrs %{name: "some updated name"}
   @invalid_attrs %{name: nil}
@@ -13,21 +14,39 @@ defmodule WilliamStorckPhxWeb.Admin.CategoryControllerTest do
     category
   end
 
+  def fixture(:admin) do
+    {:ok, admin} = Auth.create_user(@admin_attrs)
+    admin
+  end
+
+  describe "when not logged in" do
+    test "redirects to login", %{conn: conn} do
+      conn = get(conn, Routes.admin_category_path(conn, :index))
+      assert html_response(conn, 302)
+    end
+  end
+
   describe "index" do
+    setup [:sign_in_admin]
+
     test "lists all categories", %{conn: conn} do
       conn = get(conn, Routes.admin_category_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Categories"
+      assert html_response(conn, 200)
     end
   end
 
   describe "new category" do
+    setup [:sign_in_admin]
+
     test "renders form", %{conn: conn} do
       conn = get(conn, Routes.admin_category_path(conn, :new))
-      assert html_response(conn, 200) =~ "New Category"
+      assert html_response(conn, 200)
     end
   end
 
   describe "create category" do
+    setup [:sign_in_admin]
+
     test "redirects to show when data is valid", %{conn: conn} do
       conn = post(conn, Routes.admin_category_path(conn, :create), category: @create_attrs)
 
@@ -35,51 +54,59 @@ defmodule WilliamStorckPhxWeb.Admin.CategoryControllerTest do
       assert redirected_to(conn) == Routes.admin_category_path(conn, :show, id)
 
       conn = get(conn, Routes.admin_category_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show Category"
+      assert html_response(conn, 200)
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post(conn, Routes.admin_category_path(conn, :create), category: @invalid_attrs)
-      assert html_response(conn, 200) =~ "New Category"
+      assert html_response(conn, 200)
     end
   end
 
   describe "edit category" do
-    setup [:create_category]
+    setup [:sign_in_admin, :create_category]
 
     test "renders form for editing chosen category", %{conn: conn, category: category} do
       conn = get(conn, Routes.admin_category_path(conn, :edit, category))
-      assert html_response(conn, 200) =~ "Edit Category"
+      assert html_response(conn, 200)
     end
   end
 
   describe "update category" do
-    setup [:create_category]
+    setup [:sign_in_admin, :create_category]
 
     test "redirects when data is valid", %{conn: conn, category: category} do
       conn = put(conn, Routes.admin_category_path(conn, :update, category), category: @update_attrs)
       assert redirected_to(conn) == Routes.admin_category_path(conn, :show, category)
 
       conn = get(conn, Routes.admin_category_path(conn, :show, category))
-      assert html_response(conn, 200) =~ "some updated name"
+      assert html_response(conn, 200)
     end
 
     test "renders errors when data is invalid", %{conn: conn, category: category} do
       conn = put(conn, Routes.admin_category_path(conn, :update, category), category: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Edit Category"
+      assert html_response(conn, 200)
     end
   end
 
   describe "delete category" do
-    setup [:create_category]
+    setup [:sign_in_admin, :create_category]
 
     test "deletes chosen category", %{conn: conn, category: category} do
       conn = delete(conn, Routes.admin_category_path(conn, :delete, category))
       assert redirected_to(conn) == Routes.admin_category_path(conn, :index)
-      assert_error_sent 404, fn ->
-        get(conn, Routes.admin_category_path(conn, :show, category))
-      end
     end
+  end
+
+  defp sign_in_admin(_) do
+    fixture(:admin)
+
+    conn = build_conn(:get, "/")
+    conn = post(conn,
+      Routes.admin_session_path(conn, :create),
+      session: %{email: @admin_attrs.email, password: @admin_attrs.password})
+
+    {:ok, conn: conn}
   end
 
   defp create_category(_) do
