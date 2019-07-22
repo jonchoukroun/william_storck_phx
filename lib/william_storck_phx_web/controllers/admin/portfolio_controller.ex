1d0343f7c40a7831112ad586bd5b8e17e1e046e7
Jon <jonchoukroun@gmail.com>
2019-07-21 21:00:39 -0700

    Mark up 2nd portfolio option

--- lib/william_storck_phx_web/controllers/admin/portfolio_controller.ex
+++ lib/william_storck_phx_web/controllers/admin/portfolio_controller.ex
@@ -4,7 +4,12 @@ defmodule WilliamStorckPhxWeb.Admin.PortfolioController do
   alias WilliamStorckPhx.Admin
 
   def portfolio_1(conn, _params) do
-    categories = Admin.list_categories(:random)
+    categories = Admin.fetch_categories_preview(1)
     render(conn, "portfolio_1.html", categories: categories)
+  end
+
+  def portfolio_2(conn, _params) do
+    categories = Admin.fetch_categories_preview(3)
+    render(conn, "portfolio_2.html", categories: categories)
   end
 end
